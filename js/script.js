7883079210b3154215027121366fb5ad7cad308c
Harri Häivälä <haivala@cc.hut.fi>
2013-11-04 17:16:05 +0200

    ryanelliot about valmis

--- js/script.js
+++ js/script.js
@@ -34,3 +34,14 @@ $(function(){
 	});
 	
 });
+
+$("#button1").click(function() {
+//	$("#tab2").fadeOut("slow");
+	$("#tab2").hide();
+	$("#tab1").show();
+});
+$("#button2").click(function() {
+	$("#tab1").fadeOut();
+	$("#tab1").hide();
+	$("#tab2").show();
+});
