24d1135545c649d58798bb772fcd2207a1c8de2d
Harri Häivälä <haivala@cc.hut.fi>
2014-05-13 14:56:28 +0300

    vika säätö

---
+++ _plugins/relative.rb
@@ -0,0 +1,13 @@
+class Jekyll::Page
+
+  def relative
+    "../" * (url.split("/").length-2)
+  end
+
+  def to_liquid(attrs = ATTRIBUTES_FOR_LIQUID)
+    super(attrs + %w[
+          relative
+    ])
+
+  end
+end
